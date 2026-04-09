e24c125a6a5d1ad5b264ed50d3a592fd77a8f35b
RAJESH G R <rgr98624@gmail.com>
2026-04-09 23:29:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "Frontend Developer | Website Builder";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+typeEffect();
